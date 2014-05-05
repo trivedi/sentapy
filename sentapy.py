94575f13d536f2de09e892a9e5f93281645ec952
Nishad Trivedi <nishadt15@gmail.com>
2014-05-04 22:40:23 -0500

    Added colors and shebang

--- sentapy.py
+++ sentapy.py
@@ -1,15 +1,23 @@
-import NaiveBayes, twitter, analyze
+#!/usr/bin/env python
+
+from __future__ import print_function
+import twitter, analyze, os
 from NaiveBayes import c
+from termcolor import colored
+
+
 
 def menu():
 
     global flag
     flag = False
-
-    print "Sentapy - {Sentiment Analysis in Python}"
+    
+    os.system("clear")
+    print(colored("Sentapy - {Sentiment Analysis in Python}", "blue"))
+    print()
     while True:
-        input = raw_input("{menu} > ").lower()
-
+        print(colored('{menu} > ', 'yellow'), end="") 
+        input = raw_input().lower()
         if input == "classify":
             classify(flag)
         elif input == "verbose":
@@ -18,42 +26,51 @@ def menu():
             evaluate()
         elif input == "twitter":
             query()
+        elif input == "features":
+            features()
         elif input in ["exit", "quit", "q"]:
             exit()
+        elif input == "clear":
+            os.system('clear')
         else:
             help()
 
 
 def help():
-        print "Choices:"
-        print "classify\n\tClassify sentence"
-        print "twitter\n\tStream tweets and find find sentiment"
-        print "features\n\tFind top 100 feature-words with the most information gain"
-        print "evaluate\n\tFind accuracy of classifier on manually analyzed tweets"
-        print "verbose\n\tToggle flag, prints extra information"
+        print("Choices:")
+        print("classify\n\tClassify sentence")
+        print("twitter\n\tStream tweets and find find sentiment")
+        print("features\n\tFind top 100 feature-words with the most information gain")
+        print("evaluate\n\tFind accuracy of classifier on manually analyzed tweets")
+        print("verbose\n\tToggle flag, prints extra information")
 
 def classify(flag):
     while True:
-        i = raw_input("{classify} > ")
+        print(colored('{classify} > ', 'yellow'), end="") 
+        i = raw_input()
         if i in ["exit", "quit", "q", "back"]:
             break
-        c.classify(i, verbose=flag)
-
+        elif i == "clear":
+            os.system('clear')
+        else:
+            c.classify(i, verbose=flag)
 
 def evaluate():
     c.evaluate()
 
-
 def query():
     twitter.run()
+
+def features():
+    analyze.features()
 
 def verbose(flag):
     flag = not flag
     if flag:
-        print "Verbosity is [on]"
+        print("Verbosity is " + colored("[on]", "green"))
         return flag
     else:
-        print "Verbosity is [off]"
+        print("Verbosity is " + colored("[off]", "red"))
         return flag
 
 
